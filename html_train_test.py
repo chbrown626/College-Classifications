56a4c99f35563dc97a039286e45606b06c7d7743
Christianna Brown <33790693+chbrown626@users.noreply.github.com>
2018-04-30 18:14:56 -0600

    HTML Train-Test Randomization
    This code uses the train_test_split function from SciKitLearn to create randomized training and testing sets.

---
+++ html_train_test.py
@@ -0,0 +1,29 @@
+#Creating the Training & Testing Sets
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+#Create the groups the html data will be split into
+courses = ['course'] * 930
+departments = ['department'] * 182
+faculty = ['faculty'] * 1124
+others = ['other'] * 3764
+projects = ['project'] * 504
+staff = ['staff'] * 137
+students = ['student'] * 1641
+
+#Combine into one large group the same size as the clean_html data
+labels = courses + departments + faculty + others + projects + staff + students
+
+#Generate the random training and testing sets
+html_tr, html_te, categories_tr, categories_te = train_test_split(clean_html,
+                                                                  labels,
+                                                                  test_size=0.25,
+                                                                  random_state=42,
+                                                                  stratify=labels)
+
+#Review the head of each
+head(html_tr)
+head(html_te)
+head(categories_tr)
+head(categories_te)
